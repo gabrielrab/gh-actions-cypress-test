3c058b1bfd4aa3d893db2d937f53a8c44527a3b0
Gabriel Rabelo <contato.ideialisa@gmail.com>
2021-11-12 11:40:06 -0300

    chore: write index page test

--- cypress/integration/hello_world.spec.js
+++ cypress/integration/hello_world.spec.js
@@ -1,7 +1,19 @@
 /// <reference types="cypress">
 
-describe("Teste", () => {
-  it(`Fake`, () => {
-    expect(true).to.equal(true);
+describe("Application test", () => {
+  it(`Visit index page`, () => {
+    cy.visit(`/`);
+  });
+
+  it(`Validate index page texts`, () => {
+    cy.get(".App-header > :nth-child(2)").contains(`Hello World!`);
+    cy.get(".App-header > :nth-child(3)").contains(
+      `This project propose is create automated test using Cypress + React + Github Actions`
+    );
+    cy.get(".App-link").contains("Go to GitHub Repository");
+  });
+
+  it(`Visit Github repo page`, () => {
+    cy.get(".App-link").click();
   });
 });
